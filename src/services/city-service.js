bc426a8f98e622132d2ca194858ad50a5ae55e8e
Vaibhav <vjain0998@gmail.com>
2023-01-01 16:00:58 +0530

    Added city controller

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -2,11 +2,11 @@ const { CityRepository } = require('../repository/index');
 
 class CityService {
     constructor() {
-        this.CityRepository = new CityRepository();
+        this.cityRepository = new CityRepository();
     }
     async createCity(data) {
         try {
-            const city = await this.CityRepository.createCity(data);
+            const city = await this.cityRepository.createCity(data);
             return city;
         } catch (error) {
             console.log("Something went wrong at service layer");
@@ -15,7 +15,7 @@ class CityService {
     }
     async deleteCity(cityId) {
         try {
-            const response = await this.CityRepository.deleteCity(cityId);
+            const response = await this.cityRepository.deleteCity(cityId);
             return response;
         } catch (error) {
             console.log("Something went wrong at service layer");
@@ -24,7 +24,7 @@ class CityService {
     }
     async updateCity(cityId, data) {
         try {
-            const city = await this.CityRepository.updateCity(cityId, data);
+            const city = await this.cityRepository.updateCity(cityId, data);
             return city;
         } catch (error) {
             console.log("Something went wrong at service layer");
@@ -33,7 +33,7 @@ class CityService {
     }
     async getCity(cityId) {
         try {
-            const city = await this.CityRepository.getCity(cityId);
+            const city = await this.cityRepository.getCity(cityId);
             return city;
         } catch (error) {
             console.log("Something went wrong at service layer");
